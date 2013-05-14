bc33ea4cea8cdcc65eee5e62523cdfde82f43326
Gijs Molenaar <gijs@pythonic.nl>
2013-05-14 13:57:18 +0200

    fixing transient sorting

--- banana/views/tables.py
+++ banana/views/tables.py
@@ -6,6 +6,7 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from banana.db import monetdb_list, check_database
 from banana.models import Dataset, Image, Transient, Extractedsource
+from banana.tools import recur_getattr
 
 
 def databases(request):
@@ -83,45 +84,48 @@ def images(request, db_name):
     return render(request, 'images.html', context)
 
 
+transients_fields = [
+    'id',
+    'runcat__wm_ra',
+    'runcat__wm_decl',
+    't_start',
+    'siglevel',
+    'band',
+    'eta_int',
+    'v_int',
+    'runcat__datapoints',
+    'runcat'
+]
+
 def transients(request, db_name):
     check_database(db_name)
     dataset_id = request.GET.get("dataset", None)
     format = request.GET.get("format", 'html')
     order = request.GET.get('order', 'id')
-    if order not in Transient._meta.get_all_field_names():
+    order_ = order[1:] if order.startswith('-') else order
+    if order_ not in transients_fields:
         raise Http404
     related = ['band', 'runcat']
     transient_list = Transient.objects.using(db_name).prefetch_related(*related)
     if dataset_id:
         transient_list = transient_list.filter(runcat__dataset=dataset_id)
-
     transient_list = transient_list.order_by(order)
 
     if format == 'csv':
-        fields = ['id', 'runcat.wm_ra', 'runcat.wm_decl', 'startdate',
-                  'siglevel', 'band', 'eta_int', 'v_int', 'runcat.datapoints',
-                  'runcat']
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="%s_transients.csv"' % db_name
-
         writer = csv.writer(response)
-        writer.writerow(fields)
+        writer.writerow(transients_fields)
         for transient in transient_list:
-            #writer.writerow([getattr(transient, field) for field in fields])
-            writer.writerow([transient.id, transient.runcat.wm_ra,
-                             transient.runcat.wm_decl,
-                             transient.t_start, transient.siglevel,
-                             transient.band,
-                             transient.eta_int, transient.v_int,
-                             transient.runcat.datapoints, transient.runcat])
+            writer.writerow([recur_getattr(transient, field) for field in transients_fields])
         return response
     else:
         page = request.GET.get('page', 1)
         paginator = Paginator(transient_list, 100)
         transients = paginator.page(page)
-
         context = {
             'transients': transients,
+            'fields': transients_fields,
             'db_name': db_name,
             'dataset': dataset_id,
             'order': order,
