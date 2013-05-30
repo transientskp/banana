953b2dc04a7f07c3b8c49dddea2656cf8b2aee00
Gijs Molenaar <gijs@pythonic.nl>
2013-05-30 17:32:34 +0200

    always get latest version

--- banana/mongo.py
+++ banana/mongo.py
@@ -9,7 +9,7 @@ def fetch(filename):
     mongo_connection = Connection(host=settings.MONGODB["host"],
                                   port=settings.MONGODB["port"])
     gfs = GridFS(mongo_connection[settings.MONGODB["database"]])
-    return gfs.get_version(filename)
+    return gfs.get_last_version(filename)
 
 
 def get_hdu(url):
