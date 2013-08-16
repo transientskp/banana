795d759e0e4359558c710f58aedda7132eb6ddeb
Gijs Molenaar <gijs@pythonic.nl>
2013-08-16 11:54:45 +0200

    move around testing project

--- bananatest/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This app is only here for testing purposes. Since we don't want Django to
-manage our TRAP databases, we defined managed = False in the model. But
-if you want to run the test suite you need some data! So this app will
-dynamically alter the managed variable so the model is created.
-"""
---
+++ testing/settings.py
@@ -0,0 +1,15 @@
+
+from bananaproject.settings.base import *
+
+SECRET_KEY = "NSA is watching you"
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+INSTALLED_APPS += [
+    'testing',
+]
