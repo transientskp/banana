99ace0e661ff2f5f9995f10ede3cca7c1c35991d
Gijs Molenaar <gijs@pythonic.nl>
2013-08-16 12:20:07 +0200

    add more view tests

--- banana/tests.py
+++ banana/tests.py
@@ -8,16 +8,21 @@ class ViewTest(TestCase):
     """
     test if all views are working
     """
-    def test_database_list(self):
+    list_views = [
+        'datasets',
+        'images',
+        'transients',
+        'extractedsources',
+        'runningcatalogs',
+        'monitoringlists',
+    ]
+
+    def test_database_view(self):
         response = self.client.get(reverse('databases',))
         self.assertEqual(response.status_code, 200)
 
-    def test_dataset_list(self):
-        response = self.client.get(reverse('datasets', kwargs={'db': test_db}))
-        self.assertEqual(response.status_code, 200)
-
-    def test_extractedsource_list(self):
-        response = self.client.get(reverse('extractedsources',
-                                           kwargs={'db': test_db}))
-        self.assertEqual(response.status_code, 200)
-
+    def test_list_views(self):
+        for list_view in self.list_views:
+            response = self.client.get(reverse(list_view,
+                                               kwargs={'db': test_db}))
+            self.assertEqual(response.status_code, 200)
