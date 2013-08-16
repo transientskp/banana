eb9dd5c4c88f2a6a7df8356d540b8e8a1c98e37f
Gijs Molenaar <gijs@pythonic.nl>
2013-08-16 16:35:54 +0200

    this makes mongo config a bit more robust

--- banana/mongo.py
+++ banana/mongo.py
@@ -13,13 +13,18 @@ def fetch(filename):
 
 
 def get_hdu(url):
-    if settings.MONGODB["enabled"]:
+    if hasattr(settings, 'MONGODB') and settings.MONGODB["enabled"]:
         try:
             mongo_file = fetch(url)
         except NoFile:
             return None
         return pyfits.open(mongo_file, mode="readonly")
     elif os.path.exists(url):
-        return pyfits.open(url, readonly=True)
+        if os.path.isdir(url):
+            # probably a casa table
+            return None
+        else:
+            # probably a fits file
+            return pyfits.open(url, readonly=True)
     else:
         return None
