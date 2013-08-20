473436400c2fc20fdb4364bcfacac93690fd4424
Gijs Molenaar <gijs@pythonic.nl>
2013-08-20 16:29:24 +0200

    add support for manual header definition

--- banana/templatetags/tableheader.py
+++ banana/templatetags/tableheader.py
@@ -2,8 +2,11 @@ from django import template
 
 register = template.Library()
 
-def tableheader(context, field):
+def tableheader(context, field, description=False):
+    if not description:
+        description = field.title()
     context['field'] = field
+    context['description'] = description
     return context
 
 register.inclusion_tag('tableheader.html', takes_context=True)(tableheader)
