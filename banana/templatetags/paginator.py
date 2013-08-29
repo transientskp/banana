d9fdaf89184ad47f68c3119bfe32ff7e3f62beda
Gijs Molenaar <gijs@pythonic.nl>
2013-08-29 12:55:52 +0200

    fix the paginator

--- banana/templatetags/paginator.py
+++ banana/templatetags/paginator.py
@@ -3,11 +3,10 @@ from django import template
 register = template.Library()
 
 def paginatorizer(context, adjacent_pages=5):
-    page_obj = context['page_obj']
-    current_page = int(context.get('page', 1))
-    num_pages = page_obj.paginator.num_pages
-    start_page = max(current_page - adjacent_pages, 2)
-    end_page = min(current_page + adjacent_pages + 1, num_pages)
+    page = context['page_obj']
+    num_pages = page.paginator.num_pages
+    start_page = max(page.number - adjacent_pages, 2)
+    end_page = min(page.number + adjacent_pages + 1, num_pages)
     page_numbers = range(start_page, end_page)
     context['page_numbers'] = page_numbers
     if len(page_numbers) > (adjacent_pages - 1):
