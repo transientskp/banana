256df9df8e890670ccdc2a47936c5e373da3b6c6
Gijs Molenaar <gijs@pythonic.nl>
2013-09-17 14:24:16 +0200

    Sexagesimal notation for extracted source

---
+++ banana/convert.py
@@ -0,0 +1,27 @@
+import math
+
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = 60**2
+SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR
+RADIANS_IN_CIRCLE = 2 * math.pi
+ASEC_IN_AMIN = 60
+ASEC_IN_DEGREE = 60**2
+
+
+def deg_to_hms(deg):
+    rad = deg * (math.pi/180)
+    rad %= RADIANS_IN_CIRCLE
+    seconds = SECONDS_IN_DAY * rad / RADIANS_IN_CIRCLE
+    hours, seconds = divmod(seconds, SECONDS_IN_HOUR)
+    minutes, seconds = divmod(seconds, SECONDS_IN_MINUTE)
+    return hours, minutes, seconds
+
+
+def deg_to_dms(deg):
+    rad = deg * (math.pi/180)
+    sign = "+" if rad >= 0 else "-" 
+    rad = abs(rad)
+    seconds = math.degrees(rad) * ASEC_IN_DEGREE
+    degrees, seconds = divmod(seconds, ASEC_IN_DEGREE)
+    minutes, seconds = divmod(seconds, ASEC_IN_AMIN)
+    return sign, degrees, minutes, seconds
--- banana/templatetags/units.py
+++ banana/templatetags/units.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 from django import template
+from django.utils.safestring import mark_safe
+from banana.convert import deg_to_dms, deg_to_hms
 
 
 register = template.Library()
@@ -14,7 +16,7 @@ def sec2hms(seconds):
     """Seconds to hours, minutes, seconds"""
     hours, seconds = divmod(seconds, 60**2)
     minutes, seconds = divmod(seconds, 60)
-    return (int(hours), int(minutes), seconds)
+    return int(hours), int(minutes), seconds
 
 
 def dectodms(decdegs):
@@ -54,7 +56,7 @@ def dectodms(decdegs):
                 decm = -decm
         else:
             decd = -decd
-    return (decd, decm, decs)
+    return decd, decm, decs
 
 
 def ratohms(radegs):
@@ -123,6 +125,18 @@ def format_angle(value, format_type):
 def datetime2seconds(value):
     return value.strftime('%s.%f')
 
+
 @register.filter
 def datetime2miliseconds(value):
     return float(value.strftime('%s.%f')) * 1000
+
+
+@register.filter
+def format_angle(value, format_type="time"):
+    if format_type == "time":
+        h, m, s = deg_to_hms(float(value))
+        result = "%d<sup>h</sup> %d<sup>m</sup> %.1f<sup>s</sup>" % (h, m, s)
+    if format_type == "dms":
+        sign, d, m, s = deg_to_dms(float(value))
+        result = "%s%d&deg; %d&prime; %.1f&Prime;" % (sign, d, m, s)
+    return mark_safe(result)
