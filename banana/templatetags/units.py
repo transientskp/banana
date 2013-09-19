b1f950e39c0061150e8532193e795af435d91a07
Gijs Molenaar <gijs@pythonic.nl>
2013-09-19 17:10:11 +0200

    add sexagesimal position

--- banana/templatetags/units.py
+++ banana/templatetags/units.py
@@ -3,7 +3,7 @@
 from django import template
 from django.utils.safestring import mark_safe
 from banana.convert import deg_to_dms, deg_to_hms
-
+import math
 
 register = template.Library()
 
@@ -17,63 +17,6 @@ def sec2hms(seconds):
     hours, seconds = divmod(seconds, 60**2)
     minutes, seconds = divmod(seconds, 60)
     return int(hours), int(minutes), seconds
-
-
-def dectodms(decdegs):
-    """Convert Declination in decimal degrees format to hours, minutes,
-    seconds format.
-
-    Keyword arguments:
-    decdegs -- Dec. in degrees format
-
-    Return value:
-    dec -- list of 3 values, [degrees,minutes,seconds]
-
-    """
-
-    sign = -1 if decdegs < 0 else 1
-    decdegs = abs(decdegs)
-    if decdegs > 90:
-        raise ValueError("coordinate out of range")
-    decd = int(decdegs)
-    decm = int((decdegs - decd) * 60)
-    decs = (((decdegs - decd) * 60) - decm) * 60
-    # Necessary because of potential roundoff errors
-    if decs - 60 > -1e-7:
-        decm += 1
-        decs = 0
-        if decm == 60:
-            decd += 1
-            decm = 0
-            if decd > 90:
-                raise ValueError("coordinate out of range")
-
-    if sign == -1:
-        if decd == 0:
-            if decm == 0:
-                decs = -decs
-            else:
-                decm = -decm
-        else:
-            decd = -decd
-    return decd, decm, decs
-
-
-def ratohms(radegs):
-    """Convert RA in decimal degrees format to hours, minutes,
-    seconds format.
-
-    Keyword arguments:
-    radegs -- RA in degrees format
-
-    Return value:
-    ra -- tuple of 3 values, [hours,minutes,seconds]
-
-    """
-
-    radegs %= 360
-    raseconds = radegs * 3600 / 15.0
-    return sec2hms(raseconds)
 
 
 @register.filter
@@ -107,21 +50,6 @@ def mega(value, precision=3):
 
 
 @register.filter
-def format_angle(value, format_type):
-    if format_type == "time":
-        h, m, s = ratohms(value)
-        result = "%d:%d:%.1f" % (h, m, s)
-    if format_type == "dms":
-        d, m, s = dectodms(value)
-        if d > 0:
-            sign = '+'
-        else:
-            sign = '-'
-        result = "%s%d:%d:%.1f" % (sign, d, m, s)
-    return result
-
-
-@register.filter
 def datetime2seconds(value):
     return value.strftime('%s.%f')
 
@@ -133,10 +61,29 @@ def datetime2miliseconds(value):
 
 @register.filter
 def format_angle(value, format_type="time"):
+    """
+    format_type: time (hours, min, sec) or dms (degrees, arcmin, arcsec)
+    """
     if format_type == "time":
         h, m, s = deg_to_hms(float(value))
-        result = "%d<sup>h</sup> %d<sup>m</sup> %.1f<sup>s</sup>" % (h, m, s)
+        result = "%d<sup>h</sup>%d<sup>m</sup>%.1f<sup>s</sup>" % (h, m, s)
     if format_type == "dms":
         sign, d, m, s = deg_to_dms(float(value))
         result = "%s%d&deg; %d&prime; %.1f&Prime;" % (sign, d, m, s)
     return mark_safe(result)
+
+
+@register.filter
+def format_ra_error(value, decl):
+    return float(value) / 15 * math.cos(decl)
+
+
+def sexagesimal(context, ra, decl, ra_err, decl_err):
+    context['ra'] = ra
+    context['decl'] = decl
+    context['ra_err'] = ra_err
+    context['decl_err'] = decl_err
+    return context
+
+
+register.inclusion_tag('sexagesimal.html', takes_context=True)(sexagesimal)
