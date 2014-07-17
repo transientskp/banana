a03a18e2627c3fa66bf5c81d90142d57b4cc6818
Gijs Molenaar <gijs@pythonic.nl>
2014-07-17 13:12:03 +0200

    transient-NG

--- rest/urls.py
+++ rest/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from rest.routers import BananaRouter
 from rest.views import RunningcatalogViewSet, ExtractedsourceViewSet,\
-    DatasetViewSet, ImageViewSet, AssocxtrsourceViewset
+    DatasetViewSet, ImageViewSet, AssocxtrsourceViewset, TransientViewset
 
 
 router = BananaRouter()
@@ -10,6 +10,7 @@ router.register(r'extractedsources', ExtractedsourceViewSet)
 router.register(r'datasets', DatasetViewSet)
 router.register(r'images', ImageViewSet)
 router.register(r'assocxtrsources', AssocxtrsourceViewset)
+router.register(r'transients', TransientViewset)
 
 urlpatterns = patterns('',
                        url(r'', include(router.urls)),
--- rest/views.py
+++ rest/views.py
@@ -60,3 +60,52 @@ class AssocxtrsourceViewset(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Assocxtrsource.objects.all()
     serializer_class = AssocxtrsourceSerializer
+
+transient_query = """
+SELECT
+    r2.id as runcat_id,
+    a2.id as assoc_id,
+    i2.taustart_ts as timestamp,
+    i2.freq_eff as frequency,
+    a2.v_int,
+    a2.eta_int
+FROM
+    (runningcatalog r2
+    JOIN assocxtrsource a2 ON a2.runcat = r2.id
+    JOIN extractedsource e2 ON a2.xtrsrc = e2.id
+    JOIN image i2 ON e2.image = i2.id)
+    -- we need to join the query with the tables again to get the actual
+    -- relevant rows. You can't do that in one go, you first need to select
+    -- the runningcatalog ID's and the maximum timestmap per frequency.
+    JOIN (
+        -- select the latest timestamp per runningcatalog
+        SELECT
+            r.id as runcat_id,
+            i.freq_eff as frequency,
+            max(i.taustart_ts) as MaxTimestamp
+
+        FROM
+            runningcatalog r
+            JOIN assocxtrsource a ON a.runcat = r.id
+            JOIN extractedsource e ON a.xtrsrc = e.id
+            JOIN image i ON e.image = i.id
+        GROUP BY
+            runcat_id, frequency
+        ) last_timestamps
+    ON  r2.id = last_timestamps.runcat_id
+    AND i2.freq_eff = last_timestamps.frequency
+    AND i2.taustart_ts = last_timestamps.MaxTimestamp
+"""
+
+
+class TransientViewset(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows Assocxtrsources to be viewed.
+    """
+    from django.db.models import Max
+    timestamps = Assocxtrsource.objects.using('postgres_gijs')\
+        .values('runcat', 'xtrsrc__image__freq_eff')\
+        .annotate(max_timestamp=(Max('xtrsrc__image__taustart_ts')))
+
+    queryset = Runningcatalog.objects.raw(transient_query)
+    serializer_class = AssocxtrsourceSerializer
