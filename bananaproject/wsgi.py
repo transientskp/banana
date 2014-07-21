709e4ad34a60d50ec1907e8c91863a2c58677b6c
Gijs Molenaar <gijs@pythonic.nl>
2014-07-21 17:48:09 +0200

    fix wsgi script

--- bananaproject/wsgi.py
+++ bananaproject/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_monetdb_testproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bananaproject.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
