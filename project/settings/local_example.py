804fe4fda8ba2b222ee45b49ae7fd93935bbdc15
Gijs Molenaar <gijs@pythonic.nl>
2015-08-24 17:06:21 +0200

    cleanup a bit

--- project/settings/local_example.py
+++ project/settings/local_example.py
@@ -1,17 +1,12 @@
 import re
 from base import *
-from banana.db import monetdb_list, postgres_list
+from banana.db import postgres_list
 
 
 # use settings below to debug the application
 #DEBUG = True
 #INSTALLED_APPS += ['debug_toolbar']
 #TEMPLATE_DEBUG = DEBUG
-
-# settings below may be  required if you run debug toolbar with WSGI
-# http://stackoverflow.com/a/21005346/575524
-#DEBUG_TOOLBAR_PATCH_SETTINGS = False
-#MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
 
 #  always change the secret key of a django application!
@@ -21,10 +16,13 @@ SECRET_KEY = 'changeme!@'
 # always set an ADMIN email adres. in case of problem used for error reporting
 ADMINS += [('Administrator', 'change@me.nl'), ]
 
+# also set this to something valid
+SERVER_EMAIL = ADMINS[0][1]
 
 # also very important, change this to the hostname used for accessing banana
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    'localhost',
     'servername.nl',
 ]
 
@@ -55,17 +53,6 @@ for name in postgres_list(POSTGRES_HOST, POSTGRES_USERNAME, POSTGRES_PASSWORD):
         'PASSWORD': name,
     }
 
-for monetdb in monetdb_list(MONETDB_HOST, MONETDB_PORT, MONETDB_PASSPHRASE):
-    # django reverse url mapping can't handle non aplhanum chars
-    config_name = ("postgres_" + re.sub(r'\W+', '', monetdb["name"]))
-    DATABASES[config_name] = {
-        'ENGINE': 'djonet',
-        'NAME': name,
-        'USER': name,
-        'PASSWORD': name,
-        'HOST': MONETDB_HOST,
-        'PORT': MONETDB_PORT,
-    }
 
 # mongodb is used for image storage.
 MONGODB = {
