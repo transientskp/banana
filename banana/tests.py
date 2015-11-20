9b3ad95c9ad40db2f3e4c8df652812a9305e8f7a
Gijs Molenaar <gijs@pythonic.nl>
2015-11-20 14:28:55 +0200

    fix download of csv (issue #110, #109)

--- banana/tests.py
+++ banana/tests.py
@@ -68,9 +68,14 @@ class ViewTest(TestCase):
 
     def test_list_views_with_dataset(self):
         for list_view in self.list_views:
-            response = self.client.get(reverse(list_view,
-                                               kwargs={'db': test_db}) +
-                                       "?dataset=1")
+            try:
+                response = self.client.get(reverse(list_view,
+                                                   kwargs={'db': test_db}) +
+                                           "?dataset=1")
+            except Exception as e:
+                print "problem with " + list_view
+                raise
+
             self.assertEqual(response.status_code, 200)
 
     def test_detail_views(self):
